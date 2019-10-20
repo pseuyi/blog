2e2875d8d5f62a9fb14813d13a0ab6d2f225b156
pseuyi <pseuyi@gmail.com>
2019-10-19 21:07:49 -0400

    reset

--- content/blog/new-beginnings/index.md
+++ content/blog/new-beginnings/index.md
@@ -11,6 +11,10 @@ Duden flows by their place and supplies it with the necessary regelialia.
 
 ## On deer horse aboard tritely yikes and much
 
+```javascript
+if (this) return that
+```
+
 The Big Oxmox advised her not to do so, because there were thousands of bad
 Commas, wild Question Marks and devious Semikoli, but the Little Blind Text
 didn’t listen. She packed her seven versalia, put her initial into the belt and
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `/Users/freda/dev/blog`,
+    author: `freda suyi ding`,
+    description: `blog`,
+    siteUrl: `https://recursive-buckwheat.surge.sh`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pseuyi`,
     },
   },
   plugins: [
