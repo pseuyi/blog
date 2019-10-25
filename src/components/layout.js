598b353eced62a26dff7c6855d435e5b03d07b2b
pseuyi <pseuyi@gmail.com>
2019-10-25 19:48:58 -0400

    update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,7 +61,7 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>© {new Date().getFullYear()}</footer>
+        <footer>© {new Date().getFullYear()} @pseuyi</footer>
       </div>
     )
   }
