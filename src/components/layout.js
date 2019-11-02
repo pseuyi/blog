0c03ff145051078a66ef164900c7ee5042b2b120
pseuyi <pseuyi@gmail.com>
2019-11-02 16:17:51 -0400

    update link to button

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,16 +37,18 @@ class Layout extends React.Component {
             marginTop: 0,
           }}
         >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
+          <button>
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={`/`}
+            >
+              {title}
+            </Link>
+          </button>
         </h3>
       )
     }
